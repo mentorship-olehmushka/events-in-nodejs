c15a2374d32094b37bcb5bc01f40a6055f61f5d5
Oleh_Vls <olehvlasyik@gmail.com>
2020-04-09 15:22:07 +0300

    Events in node: add event emitter to calc module

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,13 @@
+const events = require('events');
+
+const operationsList = ['add', 'subtract', 'multiply', 'divide'];
 if (process.argv.length !== 5) {
   console.log('Function calc require 3 parameter:' +
     '\n 1. Operation name - one of type: "add", "subtract", "multiply", "divide"' +
     '\n 2. Number' +
     '\n 3. Number');
+} else if(!operationsList.includes(process.argv[2])) {
+  console.log('Please put correct operation name: "add", "subtract", "multiply", "divide"');
 } else {
   const args = process.argv.slice(2);
   calc(...args);
@@ -16,26 +21,11 @@ function calc(operation, arg1, arg2) {
     return;
   }
 
-  let message = '';
-  switch (operation.toLowerCase()) {
-    case 'add':
-      message = numb1 + numb2;
-      break;
-    case 'subtract':
-      message = numb1 - numb2;
-      break;
-    case 'multiply':
-      message = numb1 * numb2;
-      break;
-    case 'divide':
-      if (numb1 === 0) {
-        message = "0 can't be divided";
-      } else {
-        message = numb1 / numb2;
-      }
-      break;
-    default:
-      message = 'Please put correct operation name: "add", "subtract", "multiply", "divide"';
-  }
-  console.log(message);
+  const eventEmitter = new events.EventEmitter();
+  eventEmitter.addListener('add', () => console.log(numb1 + numb2));
+  eventEmitter.addListener('subtract', () => console.log(numb1 - numb2));
+  eventEmitter.addListener('multiply', () => console.log(numb1 * numb2));
+  eventEmitter.addListener('divide', () => console.log(numb1 === 0 ? "0 can't be divided" : numb1 / numb2));
+
+  eventEmitter.emit(operation);
 }
